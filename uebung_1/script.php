038af1decbff5e5a2715ebd371467579321f3fd8
daniel <hasenclown@googlemail.com>
2018-09-19 18:56:02 +0200

    my second commit ever

--- uebung_1/script.php
+++ uebung_1/script.php
@@ -1,16 +1,66 @@
 #!/usr/bin/php
 <?php
-//define('APACHE_MIME_TYPES_URL','http://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types');
-if (count($argv) <= 1) {
-    $dir = ".";
-} else {
-    $dir = $argv[1];
+/**
+ * ##### Listing of files and file media types #####
+ **/
+// human readable file size function
+ $dir = ".";
+function human_filesize($size, $precision = 2)
+{
+    for ($i = 0;
+         ($size / 1024) > 0.9;
+         $i++, $size /= 1024) {
+    }
+    return round($size, $precision) . ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'][$i];
 }
+
+/**
+ * get content type from argv and directory argument
+ */
+for ($i = 1; $i < $argc; $i++) {
+    switch ($argv[$i]) {
+        case "--content-type": //
+        case "-c":  //
+            $i++;
+            if ($i >= $argc) {
+                echo "Please enter a media type. \n";
+                exit;
+            }
+            $contenttype = $argv[$i];
+            break;
+        default:
+            $dir = $argv[1];
+//            var_dump($dir);
+//            die();
+            break;
+    }
+}
+// var_dump($i);
+// get dir
+//$contenttype = $argv[$i];
 $files = scandir($dir);
+// jedes element von $files wird jedes mal in $file getan
 foreach ($files as $file) {
-   /* if (!is_dir("$dir/$file")) {*/
-        echo $file . " " . filesize($dir . "/" . $file) . " " .
+    // list all files if no filter
+    if (is_dir("$dir/$file")) {
+        continue;
+    }
+    if (empty($contenttype)) {
+        //   if (mime_content_type($file==dir))
+        echo $file . " " . human_filesize(filesize($dir . "/" . $file)) . " " .
             mime_content_type($dir . "/" . $file) . "\n";
-   // }
+        continue;
+    }
+    $test = mime_content_type($dir . "/" . $file);
+    if (strpos($test, $contenttype) === 0) {
+        echo $file."\n";
+    } // erst mal nur $file
+    //elseif (strpos($test, $contenttype) === FALSE) {
+       // echo "tell mediatype \n"; // nachricht wenn kein mediatype übergeben wurde
+      //  echo "";
+
+    //  }
+   // var_dump(strpos($test, $contenttype));
+   // var_dump($test);
+   // var_dump($contenttype);
 }
-?>
